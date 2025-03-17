8591a9f95aaca8856ef1e3e5081a347cbdfb4f5b
abipriya <abipriya.rajendran@gmail.com>
2025-03-17 07:15:11 +0530

    Exercise 2

--- src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise2.java
@@ -9,11 +9,12 @@ public class Exercise2 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(System.out::println);
 
         // Print all users in the ReactiveSources.userFlux stream
-        // TODO: Write code here
+        ReactiveSources.userFlux().subscribe(System.out::println);
 
+        // This makes the program to wait and give time for the async calls to happen
         System.out.println("Press a key to end");
         System.in.read();
     }
