f92e0133d5b64d48931b780af450603a207844f9
abipriya <abipriya.rajendran@gmail.com>
2025-03-19 07:47:20 +0530

    Exercise 4

--- src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise4.java
@@ -9,13 +9,16 @@ public class Exercise4 {
         // Use ReactiveSources.intNumberMono()
 
         // Print the value from intNumberMono when it emits
-        // TODO: Write code here
+        ReactiveSources.intNumberMono().subscribe(System.out::println);
+
+        System.out.println("Press a key to proceed");
+        System.in.read();
 
         // Get the value from the Mono into an integer variable
-        // TODO: Write code here
+        // block or blockOptional can be used
+        Integer val = ReactiveSources.intNumberMono().block();
+        System.out.println("value = " + val);
 
-        System.out.println("Press a key to end");
-        System.in.read();
     }
 
 }
