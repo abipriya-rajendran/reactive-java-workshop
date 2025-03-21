01713afffbb96b03f23a4409f9d406300cd5c408
abipriya <abipriya.rajendran@gmail.com>
2025-03-21 07:25:32 +0530

    Exercise 5

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,5 +1,8 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
@@ -9,17 +12,31 @@ public class Exercise5 {
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Subscribe to a flux using the error and completion hooks
-        ReactiveSources.intNumbersFlux().subscribe(
-                num -> System.out.println(num),
-                err -> System.out.println(err.getMessage()),
-                () -> System.out.println("Complete")
-        );
+//        ReactiveSources.intNumbersFlux().subscribe(
+//                num -> System.out.println(num),
+//                err -> System.out.println(err.getMessage()),
+//                () -> System.out.println("Complete")
+//        );
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(new MySubscriber<>());
 
         System.out.println("Press a key to end");
         System.in.read();
     }
 
+}
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+
+    public void hookOnSubscribe(Subscription subscription) {
+        System.out.println("Subscribed");
+        request(2); // the moment I subscribed, I'm ready to process two items
+    }
+
+    public void hookOnNext(T value) {
+        System.out.println(value.toString() + " received"); // process current item
+        request(1); // I'm ready to process one item, push when you are ready
+    }
+    
 }
